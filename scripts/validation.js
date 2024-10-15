b486184c3e6a03e08a5a751cbc59482010618f49
ArdillaV21 <fedevalle21@gmail.com>
2024-10-15 11:54:34 -0300

    Validacion con expresiones regulares y form data

--- scripts/validation.js
+++ scripts/validation.js
@@ -6,23 +6,37 @@ const contactForm = document.getElementById("contact");
 forms.forEach(frm => { frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const form = e.target
+    const validator = []
+    const data = new FormData() //Objeto de envio
     //Funciones de utilidad
     const getField = (f) => form[f]
     const p = (n) => getField("password")[n].value
     //Verificamos que NO existe campo contraseña
     if(form.id === "contact"){
-        check(form.name.value,"text")
-        check(form.consult.value, "text")
-        check(form.phone.value, "tel")
-        check(form.email.value, "email")
+        
+            validator.push({field: "name", message:"El nombre no es valido", type:"text"})
+            validator.push({field:"email",message:"El correo es incorrecto",type:"email"})
+            validator.push({field:"phone",message:"El telefono es incorrecto",type:"tel"})
+            validator.push({field:"consult",message:"La consulta es incorrecta",type:"text"})
+            
     }
+        
     if(form.id === "signin"){
-        if(p(0) !== p(1)){
-            alert("Las contraseñas difieren")
-        }
-    }
-}
-)})
+        validator.push({field:"username", message: "El nombre de usuario no es valido"})
+        if(p(0) !== p(1)) return alert("Las contraseñas difieren")
+        validator.push({field:"password",message:"La constraseña es invalida",type:"password"})
+    } 
+    validator.forEach((v) => {
+        //Validacion HTML
+        if(!(getField(v.field).checkValidity()))
+            return alert(v.message)
+        //Validacion con RegExp
+        if(!check(getField(v.field).value,v.type))
+            return alert(v.message)
+        //Agregar valores al objeto de envio
+        data.append(v.field,typeof getField(v.field) === "object" ? getField(v.field)[0].value : getField(v.field).value)
+    })
+})})
 
 function check(value, type){
     let regExp = /\w*\d*\s*/
@@ -32,5 +46,9 @@ function check(value, type){
         regExp = /^\(\d{2,5}\)(\d{3,4}-){1,3}\d{4}$/
     if(type === "password") //Abcde1234!
         regExp= /((?=[A-Z])(?=[a-z])(?=\d)(?=[\$#!_-])){8,}/
+    if(type === "date" || type === "hour")
+        regExp= /(\d{2}[\/\-\:]{1}){2}\d{4}/
+    if(type === "card")
+        regExp = /^((?=[\s\-])\d{4}){4}$/ 
     return regExp.test(value)
 }
