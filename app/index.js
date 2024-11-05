df7df545a5a1175992755ca1dcc7092e7a557dee
ArdillaV21 <fedevalle21@gmail.com>
2024-11-04 21:41:26 -0300

    agregando la funcion render

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { getData } from "./async.js";
-import {root, links, brand} from "./variable.js"
+import {root, main, links, brand, pub, jph} from "./variable.js"
 import Nav from "./components/navigation.js";
 
 root.innerHTML += Nav(links,brand)
@@ -12,6 +12,19 @@ document.addEventListener('click', (e) => {
 
     }
     if(tag === "BUTTON"){
+        if(e.target.hasAttribute('data-url')){
+            const {url, link} = e.target.dataset
+            const server = link === "remote" ? jph : link === "local" ? pub : null;
+            history.pushState("","",url)
+            const data = server ? getData(`${server}/${url}`) : null;
 
+            render(data, url)
+        }
     }
 })
+
+function render(){
+    const content = document.createDocumentFragment()
+    main.appendChild(content)
+    root.appendChild(main)
+}
